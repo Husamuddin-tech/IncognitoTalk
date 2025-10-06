37ee54c6cf20a075569f986f3f502546e714500c
Syed Husamuddin <syedhusamuddin2@gmail.com>
2025-10-06 10:26:49 +0530

    feat: implement API endpoints for accepting messages, fetching messages, and sending messages

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,112 @@
+import dbConnect from '@/lib/dbConnect';
+import UserModel from '@/model/User';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/options';
+import { User } from 'next-auth';
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: 'Not Authenticated',
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessages: acceptMessages },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: 'Failed to update user status to accept Messages',
+        },
+        { status: 401 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: 'Message Acceptance Status Updated Successfully',
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log('Failed to update user status to accept Messages', error);
+  }
+  return Response.json(
+    {
+      success: false,
+      message: 'Failed to update user status to accept Messages',
+    },
+    { status: 500 }
+  );
+}
+
+export async function GET(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: 'Not Authenticated',
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+
+  const foundUser = await UserModel.findById(userId);
+
+  try {
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: 'User not found',
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessages,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log('Failed to fetch user message acceptance status', error);
+
+    return Response.json(
+      {
+        success: false,
+        message: 'Failed to fetch user message acceptance status',
+      },
+      { status: 500 }
+    );
+  }
+}
